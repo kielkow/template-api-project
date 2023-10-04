d96d3dc7e5e2d93db94f4b35767518b79af594ec
kielkow <matheuskiel@fiorifer.com.br>
2023-10-04 09:25:20 -0300

    feat: register users by csv file

--- src/http/controllers/users/upload/upload.ts
+++ src/http/controllers/users/upload/upload.ts
@@ -1,34 +1,54 @@
-// import { z } from 'zod'
 import fs from 'fs'
+import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
-import { UserAlreadyExistsError } from '@/use-case/errors/user-already-exists-error'
 import { convertCsvToJson } from '@/utils/convert-csv-to-json'
-// import { makeRegisterUsecase } from '@/use-case/factories/users/make-register-usecase'
+
+import { FileNotFoundError } from '@/use-case/errors/files-not-found-exists'
+import { makeRegisterUsecase } from '@/use-case/factories/users/make-register-usecase'
 
 export async function upload(request: FastifyRequest, reply: FastifyReply) {
 	try {
-		if (!request.file || !request.file.path) throw new Error('File not found')
+		if (!request.file || !request.file.path) throw new FileNotFoundError()
 
 		const filePath = request.file.path
 
 		const jsonData = await convertCsvToJson(filePath)
 
-		console.log(jsonData)
+		const registerUseCase = makeRegisterUsecase()
 
-		// const registerUseCase = makeRegisterUsecase()
-		// await registerUseCase.execute({ name, email, password })
+		const results: any = {
+			success: [],
+			failure: [],
+		}
+		for (const data of jsonData) {
+			try {
+				const registerBodySchema = z.object({
+					name: z.string(),
+					email: z.string().email(),
+					password: z.string().min(6),
+				})
+
+				const { name, email, password } = registerBodySchema.parse(data)
+
+				await registerUseCase.execute({ name, email, password })
+
+				results.success.push(data)
+			} catch (error) {
+				results.failure.push(error)
+			}
+		}
 
 		fs.unlink(request.file.path, (err) => {
 			if (err) console.error('Error deleting file', err)
 		})
+
+		return reply.status(200).send(results)
 	} catch (error) {
-		if (error instanceof UserAlreadyExistsError) {
-			return reply.status(409).send({ message: error.message })
+		if (error instanceof FileNotFoundError) {
+			return reply.status(400).send({ message: error.message })
 		}
 
 		throw error
 	}
-
-	return reply.status(201).send()
 }
