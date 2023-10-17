d08c0a15dbb6d73b328f189b82ad11b59cb11d4f
kielkow <matheuskiel@fiorifer.com.br>
2023-10-17 18:31:08 -0300

    feat: gRPC hello world server

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: [
 		'src',
+
 		'!src/grpc/proto',
 		'!src/grpc/src/proto',
+
 		'!src/**/*.spec.*',
 		'!src/**/*.yaml.*',
 		'!src/**/*.proto.*',
